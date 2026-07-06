a13ef949bbdafab0433a226a0dcbc7ba96b620c0
Your Name <your-email@example.com>
2026-07-06 12:18:18 +0530

    modified

--- parentenvironments/dev/main.tf
+++ parentenvironments/dev/main.tf
@@ -4,24 +4,9 @@ module "dev-rg-cin" {
   source = "../../childmodules/azurerm_resource_group"
   #...
 
+  
+  rgs = var.resource_group
 
-  rgs = {
-
-    rg1 = {
-
-        name = "dev-rg-cin"
-        location = "central india"
-    }
-
-    rg2 = {
-
-        name = "dev-rg-eus"
-        location = "east us"
-    }
-
-
-
-}
 
 
 }
---
+++ parentenvironments/dev/terraform.tfvars
@@ -0,0 +1,14 @@
+resource_group = {
+
+     rg1 = {
+
+        name = "dev-rg-cin"
+        location = "central india"
+    }
+
+    rg2 = {
+
+        name = "dev-rg-eus"
+        location = "east us"
+    }
+}
